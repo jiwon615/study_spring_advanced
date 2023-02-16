6fcd3cebc4d92ce4a2bd1057ae784e2b10887f7c
jiwon615 <seg615@naver.com>
2023-02-16 19:37:52 +0900

    feat: 개발한 로그 추적기 프로토타입 적용

--- src/main/java/com/study/advanced/trace/hellotrace/HelloTraceV1.java
+++ src/main/java/com/study/advanced/trace/hellotrace/HelloTraceV1.java
@@ -21,8 +21,7 @@ public class HelloTraceV1 {
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX,
-                traceId.getLevel()), message);
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
---
+++ src/main/java/com/study/advanced/v1/OrderControllerV1.java
@@ -0,0 +1,30 @@
+package com.study.advanced.v1;
+
+import com.study.advanced.trace.TraceStatus;
+import com.study.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 함 (그렇지 않으면 여기서 예외를 먹어버리고 이후에 정상 흐름으로 동작함)
+        }
+        return "ok";
+    }
+}
