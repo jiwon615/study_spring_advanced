ae48fe4128c55fbd47f563e929a460037a967f83
jiwon615 <seg615@naver.com>
2023-02-16 19:57:12 +0900

    feat: 파라미터 동기화로 로그 추적기 적용

---
+++ src/main/java/com/study/advanced/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package com.study.advanced.v2;
+
+import com.study.advanced.trace.TraceStatus;
+import com.study.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 함 (그렇지 않으면 여기서 예외를 먹어버리고 이후에 정상 흐름으로 동작함)
+        }
+        return "ok";
+    }
+}
