a1412ba3319e973fe74e201c3cba07ba720159f1
jiwon615 <seg615@naver.com>
2023-02-17 19:52:52 +0900

    test: 동시성 이슈 문제 테스트

---
+++ src/test/java/com/study/advanced/trace/threadlocal/code/FieldServiceTest.java
@@ -0,0 +1,43 @@
+package com.study.advanced.trace.threadlocal.code;
+
+import com.study.advanced.trace.threadlocal.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 동시성 문제 테스트 (A스레드 끝날때까지 대기 후 B스레드 실행하면 문제 없는 것처럼 보이지만.
+ * 거의 동시에 A,B스레드 실행하면 동시성 이슈 생기는 결과를 볼 수 있음)
+ */
+@Slf4j
+class FieldServiceTest {
+    private FieldService fieldService = new FieldService();
+    //
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start(); //A실행
+//        sleep(2000); //동시성 문제 발생X
+        sleep(100); //동시성 문제 발생O
+        threadB.start(); //B실행
+        sleep(3000); //메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
