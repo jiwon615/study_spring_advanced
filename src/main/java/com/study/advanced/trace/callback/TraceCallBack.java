915fae82ba624911deb3317366d4c26def8a6f22
jiwon615 <seg615@naver.com>
2023-02-18 14:49:08 +0900

    feat: 스프링의 템플릿 콜백 패턴 로직

---
+++ src/main/java/com/study/advanced/trace/callback/TraceCallBack.java
@@ -0,0 +1,6 @@
+package com.study.advanced.trace.callback;
+
+public interface TraceCallBack<T> {
+
+    T call();
+}
---
+++ src/main/java/com/study/advanced/v5/OrderControllerV5.java
@@ -0,0 +1,32 @@
+package com.study.advanced.v5;
+
+import com.study.advanced.trace.callback.TraceCallBack;
+import com.study.advanced.trace.callback.TraceTemplate;
+import com.study.advanced.trace.logtrace.LogTrace;
+import com.study.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template; // 적용!!
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", new TraceCallBack<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+}
